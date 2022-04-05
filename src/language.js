46c44e26e2fd348f3fd678115d3bf7fc2784771b
thrashxr <ajtroag@gmail.com>
2022-04-05 10:22:24 +0300

    main

--- src/language.js
+++ src/language.js
@@ -1,43 +1,42 @@
 const tr = {
-  settings: 'Tweet Ayarları',
-  name: 'Ad soyad',
-  username: 'Kullanıcı adı',
-  quoteTweets: 'Alıntı Tweetler',
-  likes: 'Beğeniler',
-  isVerified: 'Doğrulanmış Rozet',
-  yes: 'Evet',
-  no: 'Hayır',
-  download: 'İndir',
-  fetchInfo: 'Bilgileri Çek',
-  example: 'Örnek tweet',
-  create: 'Oluştur',
-  show: 'Göster',
-  hide: 'Gizle',
-  wLimit: 'Kelime Sınırı'
+  settings: "Tweet Ayarları",
+  name: "Ad soyad",
+  username: "Kullanıcı adı",
+  quoteTweets: "Alıntı Tweetler",
+  likes: "Beğeniler",
+  isVerified: "Doğrulanmış Rozet",
+  yes: "Evet",
+  no: "Hayır",
+  download: "İndir",
+  fetchInfo: "Bilgileri Çek",
+  example: "Örnek tweet",
+  create: "Oluştur",
+  show: "Göster",
+  hide: "Gizle",
+  wLimit: "Kelime Sınırı",
 };
 
 const en = {
-  settings: 'Tweet Settings',
-  name: 'Name and Surname',
-  username: 'Username',
-  quoteTweets: 'Quote Tweets',
-  likes: 'Likes',
-  isVerified: 'Verified Badge',
-  yes: 'Yes',
-  no: 'No',
-  download: 'Download',
-  fetchInfo: 'Get Info',
-  example: 'Example Tweet',
-  create: 'Create',
-  show: 'Show',
-  hide: 'Hide',
-  wLimit: 'Word Limit'
-  
+  settings: "Tweet Settings",
+  name: "Name and Surname",
+  username: "Username",
+  quoteTweets: "Quote Tweets",
+  likes: "Likes",
+  isVerified: "Verified Badge",
+  yes: "Yes",
+  no: "No",
+  download: "Download",
+  fetchInfo: "Get Info",
+  example: "Example Tweet",
+  create: "Create",
+  show: "Show",
+  hide: "Hide",
+  wLimit: "Word Limit",
 };
 
 const language = {
   tr,
-  en
+  en,
 };
 
 export { language };
